056b8b74f49414fa8c2f2cc5be292f3395c498a1
Miguel <angelmiguel19.14@gmail.com>
2019-04-03 15:33:46 -0400

    isauth

--- routes/api.js
+++ routes/api.js
@@ -19,7 +19,7 @@ router.get('/users', usersCtrl.getUsers);
 /* Movies  ROUTES*/
 router.get('/movies', moviesCtrl.getMovies);
 router.get('/movies/:id', moviesCtrl.getMovie);
-router.post('/movies', moviesCtrl.postNewMovie);
+router.post('/movies', isAuth, moviesCtrl.postNewMovie);
 //router.put('',moviesCtrl.putMovies);
 //router.delete('', moviesCtrl.deleteMovies);
 
@@ -29,6 +29,14 @@ router.get('/series/:id', seriesCtrl.getSerie);
 //router.post('/series', seriesCtrl.postNewSerie);
 //router.put('',seriesCtrl.putSerie);
 //router.delete('', seriesCtrl.deleteSeries);
+
+/*validar authetication*/
+function isAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/')
+};
 
 
 module.exports = router;
